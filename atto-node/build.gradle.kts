5178f74301ab43dc2ea657d6fee552be25fdda5d
Felipe <felipe@rotilho.com>
2022-12-17 17:03:46 +0100

    Improve gradle configuration

--- atto-node/build.gradle.kts
+++ atto-node/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjvm-default=enable")
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-Xjvm-default=all")
         jvmTarget = "17"
     }
 }
