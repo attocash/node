cf03e543e3480fd66afd10c872fcb7610c3674b9
Felipe <felipe@rotilho.com>
2022-12-19 17:53:29 +0100

    Move kotlin version variable to gradle plugins block

--- atto-node/build.gradle.kts
+++ atto-node/build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    val kotlinVersion = "1.7.22"
+
     id("org.springframework.boot") version "3.0.0"
     id("io.spring.dependency-management") version "1.1.0"
-    kotlin("jvm") version "1.7.22"
-    kotlin("plugin.spring") version "1.7.22"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
 }
 
 group = "com.atto"
