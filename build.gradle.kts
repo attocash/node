225431ac992cc23dffe68d7b0f3e3f5cb8bd74bc
Felipe <felipe@rotilho.com>
2023-01-29 17:56:09 +0100

    Fix publish step

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,17 +25,7 @@ subprojects {
         repositories {
             maven {
                 name = "GitHubPackages"
-                url = uri("https://maven.pkg.github.com/attomoney/node")
-//                credentials {
-//                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-//                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-//                }
             }
         }
-//        publications {
-//            register<MavenPublication>("gpr") {
-//                from(components["java"])
-//            }
-//        }
     }
 }
