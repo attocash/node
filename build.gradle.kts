7e205b0c1de806d3ce5a91cd26e52779e00148e5
rotilho <felipe@rotilho.com>
2024-09-14 15:32:55 +0200

    Initialize Inet6AddressImpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -123,6 +123,7 @@ graalvmNative {
             buildArgs.add("--strict-image-heap")
             buildArgs.add("--enable-http")
             buildArgs.add("--enable-https")
+            buildArgs.add("--initialize-at-run-time=java.net.Inet6AddressImpl,java.net.InetAddress")
         }
     }
 }
