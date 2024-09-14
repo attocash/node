bd2e82e8b49d209de0297931fe6203f341b7d9a6
rotilho <felipe@rotilho.com>
2024-09-14 16:00:51 +0200

    Prefer ipv6

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,6 +124,7 @@ graalvmNative {
             buildArgs.add("--enable-http")
             buildArgs.add("--enable-https")
             buildArgs.add("--initialize-at-run-time=java.net.Inet6AddressImpl,java.net.InetAddress")
+            buildArgs.add("-Djava.net.preferIPv6Addresses=true")
         }
     }
 }
