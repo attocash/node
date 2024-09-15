95db092c64f97d131f1a776cd7526ca23f80c83a
rotilho <felipe@rotilho.com>
2024-09-15 15:32:36 +0200

    Add more ipv6 tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -112,27 +112,15 @@ tasks.withType<Test> {
     environment("GRADLE", "true")
     useJUnitPlatform()
     maxHeapSize = "1g"
-    jvmArgs(
-        "-Djava.net.preferIPv4Stack=false",
-        "-Djava.net.preferIPv6Stack=true",
-    )
 }
 
 graalvmNative {
     binaries {
         named("main") {
-            systemProperties.put("java.net.preferIPv6Addresses", "true")
-            systemProperties.put("java.net.preferIPv4Addresses", "false")
-
             buildArgs.add("--static")
             buildArgs.add("--libc=musl")
             buildArgs.add("--gc=G1")
             buildArgs.add("--strict-image-heap")
-            buildArgs.add("--enable-http")
-            buildArgs.add("--enable-https")
-            buildArgs.add("--initialize-at-run-time=java.net.Inet4AddressImpl,java.net.Inet6AddressImpl,java.net.InetAddress")
-            buildArgs.add("-Djava.net.preferIPv6Addresses=true")
-            buildArgs.add("--enable-all-security-services")
         }
     }
 }
--- src/test/kotlin/cash/atto/node/IPv6Test.kt
+++ src/test/kotlin/cash/atto/node/IPv6Test.kt
@@ -1,17 +1,49 @@
 package cash.atto.node
 
+import io.netty.channel.nio.NioEventLoopGroup
+import io.netty.channel.socket.nio.NioDatagramChannel
+import io.netty.resolver.dns.DnsNameResolverBuilder
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.net.InetAddress
+import java.util.concurrent.TimeUnit
+import java.util.stream.Stream
 
+/**
+ * This test is just to hint graalvm to also include ipv6 classes
+ */
 class IPv6Test {
+    companion object {
+        private val ipv6Host = "ipv6.google.com"
+        private val ipv4Host = "ipv4.google.com"
 
-    /**
-     * This test is just to hint graalvm to also include ipv6 classes
-     */
-    @Test
-    fun shouldAllowIPV6() {
-        val addresses = InetAddress.getAllByName("google.com")
-        Assertions.assertEquals(2, addresses.size)
+        @JvmStatic
+        fun hosts(): Stream<String> = Stream.of(ipv4Host, ipv6Host)
+    }
+
+    @ParameterizedTest
+    @MethodSource("hosts")
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    fun `inet address resolve`(host: String) {
+        val addresses = InetAddress.getAllByName(host)
+        Assertions.assertEquals(1, addresses.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("hosts")
+    @Timeout(value = 5, unit = TimeUnit.SECONDS)
+    fun `netty should resolve`(host: String) {
+        val eventLoopGroup = NioEventLoopGroup()
+        val resolver =
+            DnsNameResolverBuilder(eventLoopGroup.next())
+                .channelType(NioDatagramChannel::class.java)
+                .build()
+
+        // Attempt to resolve an invalid domain
+        val addressFuture = resolver.resolve(ipv6Host)
+
+        addressFuture.get()
     }
 }
