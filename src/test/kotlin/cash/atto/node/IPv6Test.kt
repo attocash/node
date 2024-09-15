c6d341f82226a8c4615ae8e42bbe0eaa6716f502
rotilho <felipe@rotilho.com>
2024-09-15 20:07:15 +0200

    Use single NioEventLoopGroup

--- src/test/kotlin/cash/atto/node/IPv6Test.kt
+++ src/test/kotlin/cash/atto/node/IPv6Test.kt
@@ -3,6 +3,7 @@ package cash.atto.node
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioDatagramChannel
 import io.netty.resolver.dns.DnsNameResolverBuilder
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Timeout
 import org.junit.jupiter.params.ParameterizedTest
@@ -18,6 +19,14 @@ class IPv6Test {
     companion object {
         private val ipv6Host = "ipv6.google.com"
         private val ipv4Host = "ipv4.google.com"
+
+        val eventLoopGroup = NioEventLoopGroup()
+
+        @AfterAll
+        @JvmStatic
+        fun afterAll() {
+            eventLoopGroup.close()
+        }
 
         @JvmStatic
         fun hosts(): Stream<String> = Stream.of(ipv4Host, ipv6Host)
@@ -35,13 +44,11 @@ class IPv6Test {
     @MethodSource("hosts")
     @Timeout(value = 5, unit = TimeUnit.SECONDS)
     fun `netty should resolve`(host: String) {
-        val eventLoopGroup = NioEventLoopGroup()
         val resolver =
             DnsNameResolverBuilder(eventLoopGroup.next())
                 .channelType(NioDatagramChannel::class.java)
                 .build()
 
-        // Attempt to resolve an invalid domain
         val addressFuture = resolver.resolve(ipv6Host)
 
         addressFuture.get()
