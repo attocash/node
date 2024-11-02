c5988264274ce5cdcb527a1ea147f86d4632d5bd
rotilho <felipe@rotilho.com>
2024-11-02 09:55:58 +0100

    Fix Remote Signer

---
+++ src/main/kotlin/cash/atto/node/signature/SignatureConfiguration.kt
@@ -0,0 +1,36 @@
+package cash.atto.node.signature
+
+import cash.atto.commons.AttoPrivateKey
+import cash.atto.commons.AttoSigner
+import cash.atto.commons.fromHexToByteArray
+import cash.atto.commons.signer.remote
+import cash.atto.commons.toHex
+import cash.atto.commons.toSigner
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SignatureConfiguration {
+    private val logger = KotlinLogging.logger {}
+
+    @Bean
+    fun signer(signerProperties: SignerProperties): AttoSigner {
+        if (signerProperties.backend == SignerProperties.Backend.REMOTE) {
+            return AttoSigner.remote(signerProperties.remoteUrl!!) {
+                mapOf("Authorization" to signerProperties.token!!)
+            }
+        }
+
+        val privateKey =
+            if (!signerProperties.key.isNullOrEmpty()) {
+                AttoPrivateKey(signerProperties.key!!.fromHexToByteArray())
+            } else {
+                val temporaryPrivateKey = AttoPrivateKey.generate()
+                logger.info { "No private key configured. Created TEMPORARY private key ${temporaryPrivateKey.value.toHex()}. DO NOT USE IT!" }
+                temporaryPrivateKey
+            }
+
+        return privateKey.toSigner()
+    }
+}
