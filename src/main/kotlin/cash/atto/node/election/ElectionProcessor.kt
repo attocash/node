e99b18d6db3ecc5c8acde2422a9958b4945d42f4
rotilho <felipe@rotilho.com>
2024-12-08 10:07:30 +0100

    Batch save transactions and account

--- src/main/kotlin/cash/atto/node/election/ElectionProcessor.kt
+++ src/main/kotlin/cash/atto/node/election/ElectionProcessor.kt
@@ -9,11 +9,15 @@ import cash.atto.node.vote.VoteService
 import cash.atto.protocol.AttoNode
 import cash.atto.protocol.AttoTransactionPush
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.channels.Channel
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
 
 @Service
-class ElectionMonitor(
+class ElectionProcessor(
     private val thisNode: AttoNode,
     private val messagePublisher: NetworkMessagePublisher,
     private val accountService: AccountService,
@@ -21,16 +25,7 @@ class ElectionMonitor(
 ) {
     private val logger = KotlinLogging.logger {}
 
-    @EventListener
-    suspend fun process(event: ElectionConsensusReached) {
-        val transaction = event.transaction
-        val votes = event.votes
-
-        accountService.add(TransactionSource.ELECTION, transaction)
-        if (thisNode.isHistorical()) {
-            voteService.saveAll(votes)
-        }
-    }
+    private val buffer = Channel<ElectionConsensusReached>(Channel.UNLIMITED)
 
     @EventListener
     fun process(event: ElectionExpiring) {
@@ -46,5 +41,27 @@ class ElectionMonitor(
                 transactionPush,
             ),
         )
+    }
+
+    @EventListener
+    suspend fun process(event: ElectionConsensusReached) {
+        buffer.send(event)
+    }
+
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.MILLISECONDS)
+    @Transactional
+    suspend fun flush() {
+        for (i in 1..1000) {
+            val event = buffer.tryReceive().getOrNull() ?: break
+
+            val transaction = event.transaction
+            val votes = event.votes
+
+            accountService.add(TransactionSource.ELECTION, transaction)
+
+            if (thisNode.isHistorical()) {
+                voteService.saveAll(votes)
+            }
+        }
     }
 }
