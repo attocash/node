142bde22c7cc8ebdf60905e99f3490c3e558c6b0
rotilho <felipe@rotilho.com>
2024-12-15 10:13:36 +0100

    Improve vote refreshing

---
+++ src/main/kotlin/cash/atto/node/vote/weight/WeightService.kt
@@ -0,0 +1,19 @@
+package cash.atto.node.vote.weight
+
+import kotlinx.coroutines.flow.Flow
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.concurrent.TimeUnit
+
+@Service
+class WeightService(private val weightRepository: WeightRepository) {
+
+    @Transactional
+    @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.HOURS)
+    suspend fun refresh(): Flow<Weight> {
+        weightRepository.deleteAll()
+        weightRepository.refreshWeights()
+        return weightRepository.findAll()
+    }
+}
