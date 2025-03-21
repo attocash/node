4ea2afb652531a6c8e40cc1fb5b2c0aac977496b
Felipe Rotilho <felipe@rotilho.com>
2025-03-21 10:12:07 +0100

    Fix node schema (#251)

--- src/main/kotlin/cash/atto/node/transaction/TransactionController.kt
+++ src/main/kotlin/cash/atto/node/transaction/TransactionController.kt
@@ -1,6 +1,8 @@
 package cash.atto.node.transaction
 
+import cash.atto.commons.AttoAlgorithm
 import cash.atto.commons.AttoHash
+import cash.atto.commons.AttoNetwork
 import cash.atto.commons.AttoPublicKey
 import cash.atto.commons.AttoTransaction
 import cash.atto.commons.toAttoHeight
@@ -16,6 +18,8 @@ import cash.atto.node.sortByHeight
 import cash.atto.node.toBigInteger
 import cash.atto.protocol.AttoNode
 import cash.atto.protocol.AttoTransactionPush
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.media.Content
@@ -46,6 +50,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import java.math.BigDecimal
 import java.net.InetSocketAddress
 
 @RestController
@@ -85,7 +90,7 @@ class TransactionController(
                 content = [
                     Content(
                         mediaType = MediaType.APPLICATION_NDJSON_VALUE,
-                        schema = Schema(implementation = AttoTransaction::class),
+                        schema = Schema(implementation = AttoTransactionSample::class),
                     ),
                 ],
             ),
@@ -97,7 +102,20 @@ class TransactionController(
             .onCompletion { logger.trace { "Stopped streaming latest transactions" } }
 
     @GetMapping("/transactions/{hash}")
-    @Operation(description = "Get transaction")
+    @Operation(
+        summary = "Get transaction",
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+                content = [
+                    Content(
+                        mediaType = MediaType.APPLICATION_NDJSON_VALUE,
+                        schema = Schema(implementation = AttoTransactionSample::class),
+                    ),
+                ],
+            ),
+        ],
+    )
     suspend fun get(
         @PathVariable hash: AttoHash,
     ): ResponseEntity<AttoTransaction> {
@@ -114,7 +132,7 @@ class TransactionController(
                 content = [
                     Content(
                         mediaType = MediaType.APPLICATION_NDJSON_VALUE,
-                        schema = Schema(implementation = AttoTransaction::class),
+                        schema = Schema(implementation = AttoTransactionSample::class),
                     ),
                 ],
             ),
@@ -150,7 +168,7 @@ class TransactionController(
                 content = [
                     Content(
                         mediaType = MediaType.APPLICATION_NDJSON_VALUE,
-                        schema = Schema(implementation = AttoTransaction::class),
+                        schema = Schema(implementation = AttoTransactionSample::class),
                     ),
                 ],
             ),
@@ -193,7 +211,22 @@ class TransactionController(
     }
 
     @PostMapping("/transactions", consumes = [MediaType.APPLICATION_JSON_VALUE])
-    @Operation(description = "Publish transaction")
+    @Operation(
+        summary = "Publish a transaction",
+        requestBody = io.swagger.v3.oas.annotations.parameters.RequestBody(
+            content = [
+                Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = Schema(implementation = AttoTransactionSample::class),
+                )
+            ]
+        ),
+        responses = [
+            ApiResponse(
+                responseCode = "200",
+            ),
+        ],
+    )
     suspend fun publish(
         @RequestBody transaction: AttoTransaction,
         request: ServerHttpRequest,
@@ -253,4 +286,162 @@ class TransactionController(
     override fun clear() {
         transactionFlow.resetReplayCache()
     }
+
+
+    @Schema(name = "AttoBlock", description = "Base type for all block variants")
+    @JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type"  // this must match the "type" field in your JSON
+    )
+    @JsonSubTypes(
+        JsonSubTypes.Type(value = AttoSendBlockSample::class, name = "SEND"),
+        JsonSubTypes.Type(value = AttoReceiveBlockSample::class, name = "RECEIVE"),
+        JsonSubTypes.Type(value = AttoOpenBlockSample::class, name = "OPEN"),
+        JsonSubTypes.Type(value = AttoChangeBlockSample::class, name = "CHANGE")
+    )
+    sealed interface AttoBlockSample {
+        val network: AttoNetwork
+        val version: Int
+        val algorithm: AttoAlgorithm
+        val publicKey: String
+        val balance: BigDecimal
+        val timestamp: Long
+    }
+
+    @Schema(name = "AttoSendBlock", description = "Represents a SEND block")
+    data class AttoSendBlockSample(
+        override val network: AttoNetwork,
+
+        override val version: Int,
+
+        override val algorithm: AttoAlgorithm,
+
+        override val publicKey: String,
+
+        override val balance: BigDecimal,
+
+        override val timestamp: Long,
+
+        @Schema(description = "Height of the block", example = "2")
+        val height: BigDecimal,
+
+        @Schema(description = "Hash of the previous block", example = "6CC2D3A7513723B1BA59DE784BA546BAF6447464D0BA3D80004752D6F9F4BA23")
+        val previous: String,
+
+        @Schema(description = "Algorithm of the receiver", example = "V1")
+        val receiverAlgorithm: AttoAlgorithm,
+
+        @Schema(description = "Public key of the receiver", example = "552254E101B51B22080D084C12C94BF7DFC5BE0D973025D62C0BC1FF4D9B145F")
+        val receiverPublicKey: String,
+
+        @Schema(description = "Amount being sent", example = "1")
+        val amount: BigDecimal
+    ) : AttoBlockSample
+
+    @Schema(name = "AttoReceiveBlock", description = "Represents a RECEIVE block")
+    data class AttoReceiveBlockSample(
+        override val network: AttoNetwork,
+
+        override val version: Int,
+
+        override val algorithm: AttoAlgorithm,
+
+        override val publicKey: String,
+
+        override val balance: BigDecimal,
+
+        override val timestamp: Long,
+
+        @Schema(description = "Height of the block", example = "2")
+        val height: BigDecimal,
+
+        @Schema(description = "Hash of the previous block", example = "03783A08F51486A66A602439D9164894F07F150B548911086DAE4E4F57A9C4DD")
+        val previous: String,
+
+        @Schema(description = "Algorithm of the send block", example = "V1")
+        val sendHashAlgorithm: AttoAlgorithm,
+
+        @Schema(description = "Hash of the send block", example = "EE5FDA9A1ACEC7A09231792C345CDF5CD29F1059E5C413535D9FCA66A1FB2F49")
+        val sendHash: String
+    ) : AttoBlockSample
+
+    @Schema(name = "AttoOpenBlock", description = "Represents an OPEN block")
+    data class AttoOpenBlockSample(
+        override val network: AttoNetwork,
+
+        override val version: Int,
+
+        override val algorithm: AttoAlgorithm,
+
+        override val publicKey: String,
+
+        override val balance: BigDecimal,
+
+        override val timestamp: Long,
+
+        @Schema(description = "Algorithm of the send block", example = "V1")
+        val sendHashAlgorithm: AttoAlgorithm,
+
+        @Schema(description = "Hash of the send block", example = "4DC7257C0F492B8C7AC2D8DE4A6DC4078B060BB42FDB6F8032A839AAA9048DB0")
+        val sendHash: String,
+
+        @Schema(description = "Algorithm of the representative", example = "V1")
+        val representativeAlgorithm: AttoAlgorithm,
+
+        @Schema(
+            description = "Public key of the representative",
+            example = "69C010A8A74924D083D1FC8234861B4B357530F42341484B4EBDA6B99F047105"
+        )
+        val representativePublicKey: String
+    ) : AttoBlockSample
+
+    @Schema(name = "AttoChangeBlock", description = "Represents a CHANGE block")
+    data class AttoChangeBlockSample(
+        override val network: AttoNetwork,
+
+        override val version: Int,
+
+        override val algorithm: AttoAlgorithm,
+
+        override val publicKey: String,
+
+        override val balance: BigDecimal,
+
+        override val timestamp: Long,
+
+        @Schema(description = "Height of the block", example = "2")
+        val height: BigDecimal,
+
+        @Schema(description = "Hash of the previous block", example = "AD675BD718F3D96F9B89C58A8BF80741D5EDB6741D235B070D56E84098894DD5")
+        val previous: String,
+
+        @Schema(description = "Algorithm of the representative", example = "V1")
+        val representativeAlgorithm: AttoAlgorithm,
+
+        @Schema(
+            description = "Public key of the representative",
+            example = "69C010A8A74924D083D1FC8234861B4B357530F42341484B4EBDA6B99F047105"
+        )
+        val representativePublicKey: String
+    ) : AttoBlockSample
+
+    @Schema(name = "AttoTransaction", description = "A signed block")
+    data class AttoTransactionSample(
+
+        @Schema(description = "The block to be submitted (SEND, RECEIVE, OPEN, CHANGE)")
+        val block: AttoBlockSample,
+
+        @Schema(
+            description = "Ed25519 signature of the block",
+            example = "52843B36ABDFA4125E4C0D465A3C976C269F993C7C82645B29AB49B7A5A84FC41E3391D2A41C4CB83DFA3214DA87B099F86EF10402BFB1120A5D34F70CBC2B00"
+        )
+        val signature: String,
+
+        @Schema(
+            description = "Proof-of-work for the block",
+            example = "4300FFFFFFFFFFCF"
+        )
+        val work: String
+    )
 }
