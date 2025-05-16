cb9ebc96595a331123e32ffd76b2b36c845abd7e
rotilho <felipe@rotilho.com>
2025-05-16 08:45:00 +0200

    Return invalid send if overflows or underflows

--- src/main/kotlin/cash/atto/node/transaction/validation/validator/SendValidator.kt
+++ src/main/kotlin/cash/atto/node/transaction/validation/validator/SendValidator.kt
@@ -17,13 +17,23 @@ class SendValidator : TransactionValidator {
         transaction: Transaction,
     ): TransactionViolation? {
         val block = transaction.block as AttoSendBlock
-        if (account.balance != block.balance + block.amount) {
+
+        try {
+            if (account.balance != block.balance + block.amount) {
+                return TransactionViolation(
+                    TransactionRejectionReason.INVALID_AMOUNT,
+                    "The account ${account.publicKey} balance is ${account.balance}. " +
+                        "The received send transaction has the amount ${block.amount}",
+                )
+            }
+        } catch (e: Exception) {
             return TransactionViolation(
                 TransactionRejectionReason.INVALID_AMOUNT,
                 "The account ${account.publicKey} balance is ${account.balance}. " +
                     "The received send transaction has the amount ${block.amount}",
             )
         }
+
         return null
     }
 }
