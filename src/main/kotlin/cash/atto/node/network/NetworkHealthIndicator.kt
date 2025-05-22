13510f7c2a2c84969af24d78fea61ef5824ba4c4
rotilho <felipe@rotilho.com>
2025-05-22 08:31:49 +0200

    Refactor peer tracking to use AtomicInteger for count

--- src/main/kotlin/cash/atto/node/network/NetworkHealthIndicator.kt
+++ src/main/kotlin/cash/atto/node/network/NetworkHealthIndicator.kt
@@ -5,38 +5,31 @@ import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
-import java.net.URI
 import java.time.Duration
 import java.time.Instant
-import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicInteger
 
 @Component
 class NetworkHealthIndicator : HealthIndicator {
     private val logger = KotlinLogging.logger {}
 
-    private val peers = ConcurrentHashMap.newKeySet<URI>()
-
+    private val peerCount = AtomicInteger(0)
     private var lastDisconnect = Instant.now()
 
     @EventListener
     fun add(nodeEvent: NodeConnected) {
-        val node = nodeEvent.node
-        if (!node.isHistorical()) {
-            return
-        }
-        peers.add(node.publicUri)
+        peerCount.incrementAndGet()
     }
 
     @EventListener
     fun remove(nodeEvent: NodeDisconnected) {
-        val node = nodeEvent.node
-        peers.remove(node.publicUri)
-        lastDisconnect = Instant.now()
+        peerCount.decrementAndGet()
+
     }
 
     override fun health(): Health {
-        return if (peers.isNotEmpty() || lastDisconnect > Instant.now().minus(Duration.ofMinutes(5))) {
-            Health.up().withDetail("peers", peers.size.toString()).build()
+        return if (peerCount.get() > 0 || lastDisconnect > Instant.now().minus(Duration.ofMinutes(5))) {
+            Health.up().withDetail("peers", peerCount.get().toString()).build()
         } else {
             logger.warn { "Node is not connected to any peer" }
             Health.down().build()
