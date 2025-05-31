b34712d73d5f6d62b4ea5dd1aa17db982146525c
rotilho <felipe@rotilho.com>
2025-05-31 14:44:47 +0200

    Use NodeConnectionManager instead of peerCount directly

--- src/main/kotlin/cash/atto/node/network/NetworkHealthIndicator.kt
+++ src/main/kotlin/cash/atto/node/network/NetworkHealthIndicator.kt
@@ -3,32 +3,22 @@ package cash.atto.node.network
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
-import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import java.time.Duration
 import java.time.Instant
-import java.util.concurrent.atomic.AtomicInteger
 
 @Component
-class NetworkHealthIndicator : HealthIndicator {
+class NetworkHealthIndicator(
+    private val connectionManager: NodeConnectionManager
+) : HealthIndicator {
     private val logger = KotlinLogging.logger {}
 
-    private val peerCount = AtomicInteger(0)
     private var lastDisconnect = Instant.now()
 
-    @EventListener
-    fun add(nodeEvent: NodeConnected) {
-        peerCount.incrementAndGet()
-    }
-
-    @EventListener
-    fun remove(nodeEvent: NodeDisconnected) {
-        peerCount.decrementAndGet()
-    }
 
     override fun health(): Health =
-        if (peerCount.get() > 0 || lastDisconnect > Instant.now().minus(Duration.ofMinutes(5))) {
-            Health.up().withDetail("peers", peerCount.get().toString()).build()
+        if (connectionManager.connectionCount > 0 || lastDisconnect > Instant.now().minus(Duration.ofMinutes(5))) {
+            Health.up().withDetail("peers", connectionManager.connectionCount.toString()).build()
         } else {
             logger.warn { "Node is not connected to any peer" }
             Health.down().build()
