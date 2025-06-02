5f4c988b80321da72bb6068c696e475adc7a1ade
rotilho <felipe@rotilho.com>
2025-06-02 20:01:06 +0200

    Add node version metric

---
+++ src/main/kotlin/cash/atto/node/NodeMetricProvider.kt
@@ -0,0 +1,36 @@
+package cash.atto.node
+
+import cash.atto.commons.AttoAddress
+import cash.atto.protocol.AttoNode
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class NodeMetricProvider(
+    private val thisNode: AttoNode,
+    @Value("\${spring.application.version:local}") private val applicationVersion: String,
+    private val meterRegistry: MeterRegistry,
+) {
+    @PostConstruct
+    fun start() {
+        val version = applicationVersion.substringBefore('-').toDoubleOrNull() ?: 0.0
+        Gauge.builder("node.version", this) { version }
+            .description("Information about the Atto node")
+            .tags(
+                listOf(
+                    Tag.of("public_uri", thisNode.publicUri.toString()),
+                    Tag.of("network", thisNode.network.toString()),
+                    Tag.of("public_key", thisNode.publicKey.toString()),
+                    Tag.of("algorithm", thisNode.algorithm.toString()),
+                    Tag.of("address", AttoAddress(thisNode.algorithm, thisNode.publicKey).toString()),
+                    Tag.of("features", thisNode.features.joinToString(", ")),
+                    Tag.of("test_version", version.toString())
+                )
+            )
+            .register(meterRegistry)
+    }
+}
