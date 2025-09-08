950f544c399d3690896fb46f1cd7a8366a48aa80
rotilho <felipe@rotilho.com>
2025-09-08 13:28:36 +0200

    Bump `commonsVersion` to 5.2.0 in dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ ext["kotlin-coroutines.version"] = "1.9.0"
 ext["kotlin-serialization.version"] = "1.8.0"
 
 dependencies {
-    val commonsVersion = "5.1.1"
+    val commonsVersion = "5.2.0"
     val cucumberVersion = "7.27.2"
     val springdocVersion = "2.8.10"
     val ktorVersion = "3.2.3"
