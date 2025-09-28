b38f582679e5903816bc4e40ccab73911505c798
rotilho <felipe@rotilho.com>
2025-09-28 18:11:30 +0200

    Introduce `UncheckedTransactionInserter` to batch insert transactions

--- src/main/kotlin/cash/atto/node/bootstrap/unchecked/UncheckedTransactionService.kt
+++ src/main/kotlin/cash/atto/node/bootstrap/unchecked/UncheckedTransactionService.kt
@@ -4,33 +4,22 @@ import cash.atto.node.EventPublisher
 import cash.atto.node.bootstrap.UncheckedTransactionSaved
 import cash.atto.node.executeAfterCommit
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
-import kotlinx.coroutines.flow.toSet
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Isolation
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 class UncheckedTransactionService(
     private val uncheckedTransactionRepository: UncheckedTransactionRepository,
+    private val uncheckedTransactionInserter: UncheckedTransactionInserter,
     private val eventPublisher: EventPublisher,
 ) {
     private val logger = KotlinLogging.logger {}
 
-    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @Transactional
     suspend fun save(uncheckedTransactions: Collection<UncheckedTransaction>) {
-        val existingTransactions =
-            uncheckedTransactionRepository
-                .findAllById(uncheckedTransactions.map { it.id })
-                .map { it.hash }
-                .toSet()
-
-        val transactionsToSave = uncheckedTransactions.filter { it.hash !in existingTransactions }
-
-        val savedTransactions = uncheckedTransactionRepository.saveAll(transactionsToSave).toList()
+        uncheckedTransactionInserter.insert(uncheckedTransactions)
         executeAfterCommit {
-            savedTransactions.forEach {
+            uncheckedTransactions.forEach {
                 logger.debug { "Saved $it" }
                 eventPublisher.publish(UncheckedTransactionSaved(it))
             }
