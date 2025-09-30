b02bfd45ec5685f3c491f1b43ba6686c81b618f2
rotilho <950510+rotilho@users.noreply.github.com>
2025-09-30 09:23:44 +0200

    Fix findGaps query to return just the first gap for a given public key

--- src/main/kotlin/cash/atto/node/bootstrap/unchecked/UncheckedTransactionRepository.kt
+++ src/main/kotlin/cash/atto/node/bootstrap/unchecked/UncheckedTransactionRepository.kt
@@ -27,30 +27,35 @@ interface UncheckedTransactionRepository :
 
     @Query(
         """
-            WITH max_transaction_height AS (
-                SELECT public_key, COALESCE(MAX(height), 0) AS max_height
-                FROM transaction
-                GROUP BY public_key
-            ),
-            calculated_gaps AS (
+            WITH calculated_gaps AS (
                 SELECT
                     ut.public_key,
                     GREATEST(
-                        COALESCE(mth.max_height, 0),
+                        COALESCE(a.height, 0),
                         COALESCE(LAG(ut.height) OVER (PARTITION BY ut.public_key ORDER BY ut.height ASC), 0)
                     ) + 1 AS start_height,
                     ut.height - 1 AS end_height,
                     ut.previous AS expected_end_hash,
                     ut.timestamp AS transaction_timestamp
                 FROM unchecked_transaction ut
-                LEFT JOIN max_transaction_height mth
-                    ON ut.public_key = mth.public_key
+                LEFT JOIN account a ON a.public_key = ut.public_key
+                WHERE ut.height > COALESCE(a.height, 0)
+                AND public_key NOT IN (:publicKeyToExclude)
+
+            ),
+            ranked AS (
+              SELECT
+                cg.*,
+                ROW_NUMBER() OVER (
+                  PARTITION BY cg.public_key
+                  ORDER BY cg.transaction_timestamp ASC
+                ) AS rn
+              FROM calculated_gaps cg
+              WHERE cg.start_height <= cg.end_height
             )
             SELECT public_key, start_height, end_height, expected_end_hash
-            FROM calculated_gaps
-            WHERE start_height <= end_height
-            AND public_key not in (:publicKeyToExclude)
-            ORDER BY transaction_timestamp
+            FROM ranked
+            WHERE rn = 1
             LIMIT :limit;
         """,
     )
