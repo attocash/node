455d206d1af6e8f621a769637cdadce4b1bedb76
rotilho <felipe@rotilho.com>
2025-09-30 17:04:17 +0200

    Fix log to correctly display the size of the last completed gaps in `GapDiscoverer`

--- src/main/kotlin/cash/atto/node/bootstrap/discovery/GapDiscoverer.kt
+++ src/main/kotlin/cash/atto/node/bootstrap/discovery/GapDiscoverer.kt
@@ -117,7 +117,7 @@ class GapDiscoverer(
                 return
             }
 
-            logger.trace { "Pointer map size is ${pointerMap.size} and last completed gap is $lastCompletedGaps. Looking for $limit more gaps" }
+            logger.trace { "Pointer map size is ${pointerMap.size} and last completed gap is ${lastCompletedGaps.size}. Looking for $limit more gaps" }
 
             val publicKeyToExclude =
                 (pointerMap.keys + lastCompletedGaps.keys)
