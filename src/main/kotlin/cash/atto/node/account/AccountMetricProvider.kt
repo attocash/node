3932d73eef453eaf36711efcce6fca76bd189e85
rotilho <felipe@rotilho.com>
2025-10-01 14:20:10 +0200

    Switch to `runBlocking` for `AccountMetricProvider` initialization tasks

--- src/main/kotlin/cash/atto/node/account/AccountMetricProvider.kt
+++ src/main/kotlin/cash/atto/node/account/AccountMetricProvider.kt
@@ -1,18 +1,14 @@
 package cash.atto.node.account
 
-import io.github.oshai.kotlinlogging.KotlinLogging
 import io.micrometer.core.instrument.Gauge
 import io.micrometer.core.instrument.MeterRegistry
 import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.DelicateCoroutinesApi
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.springframework.context.annotation.DependsOn
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import java.util.concurrent.atomic.AtomicLong
-import kotlin.time.Duration.Companion.minutes
 
 @Component
 @DependsOn("flywayInitializer")
@@ -20,8 +16,6 @@ class AccountMetricProvider(
     private val repository: AccountCrudRepository,
     private val meterRegistry: MeterRegistry,
 ) {
-    private val logger = KotlinLogging.logger {}
-
     private val count = AtomicLong(0)
 
     @OptIn(DelicateCoroutinesApi::class)
@@ -31,16 +25,9 @@ class AccountMetricProvider(
             .builder("account.height.count") { count.get() }
             .description("Current sum of all account heights")
             .register(meterRegistry)
-        GlobalScope.launch {
-            while (true) {
-                try {
-                    count.addAndGet(repository.sumHeight())
-                    return@launch
-                } catch (e: Exception) {
-                    logger.error(e) { "Error while getting total height count from database" }
-                    delay(1.minutes)
-                }
-            }
+
+        runBlocking {
+            count.addAndGet(repository.sumHeight())
         }
     }
 
