18b7f6042a240db2429d2cd44f7256a0090105c7
rotilho <felipe@rotilho.com>
2025-12-09 22:50:56 +0100

    Bump commons

--- build.gradle.kts
+++ build.gradle.kts
@@ -45,7 +45,7 @@ ext["kotlin-coroutines.version"] = "1.9.0"
 ext["kotlin-serialization.version"] = "1.8.0"
 
 dependencies {
-    val commonsVersion = "ab19ef8-SNAPSHOT"
+    val commonsVersion = "6.1.0"
     val cucumberVersion = "7.32.0"
     val springdocVersion = "2.8.14"
     val ktorVersion = "3.3.3"
