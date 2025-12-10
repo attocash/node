f9f2ba19dcb2582aed3804a597a16967112167be
rotilho <felipe@rotilho.com>
2025-12-10 16:29:34 +0100

    Bump Spring Boot version to 3.5.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
-    id("org.springframework.boot") version "3.5.6"
+    id("org.springframework.boot") version "3.5.8"
     id("org.graalvm.buildtools.native") version "0.11.3"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
 }
