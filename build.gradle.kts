5ca32886d471b6d2e12003941a0eae8f7f073a7a
rotilho <felipe@rotilho.com>
2026-03-08 08:03:29 +0100

    Add Foojay resolver plugin to build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id("org.springframework.boot") version "3.5.8"
     id("org.graalvm.buildtools.native") version "0.11.4"
     id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 group = "cash.atto"
