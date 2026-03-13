7dee70a324fdcf0e13ea37dccc37cf84f4522bcb
rotilho <felipe@rotilho.com>
2026-03-13 21:04:32 +0100

    Use timers for election vote latency metrics

--- src/main/kotlin/cash/atto/node/election/ElectionMetricProvider.kt
+++ src/main/kotlin/cash/atto/node/election/ElectionMetricProvider.kt
@@ -3,25 +3,33 @@ package cash.atto.node.election
 import cash.atto.node.vote.VoteValidated
 import cash.atto.protocol.AttoNode
 import io.micrometer.core.instrument.MeterRegistry
-import io.micrometer.core.instrument.Tags
+import io.micrometer.core.instrument.Timer
 import jakarta.annotation.PostConstruct
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import java.time.Duration
-import java.util.concurrent.atomic.AtomicReference
 
 @Component
 class ElectionMetricProvider(
     private val thisNode: AttoNode,
     private val meterRegistry: MeterRegistry,
 ) {
-    private val lastVoteLatency = AtomicReference(Duration.ZERO)
-    private val lastFinalVoteLatency = AtomicReference(Duration.ZERO)
+    private lateinit var voteLatencyTimer: Timer
+    private lateinit var finalVoteLatencyTimer: Timer
 
     @PostConstruct
     fun start() {
-        meterRegistry.gauge("election.vote.latency", Tags.of("type", "non-final"), lastVoteLatency) { it.get().toMillis().toDouble() }
-        meterRegistry.gauge("election.vote.latency", Tags.of("type", "final"), lastFinalVoteLatency) { it.get().toMillis().toDouble() }
+        voteLatencyTimer =
+            Timer
+                .builder("election.vote.latency")
+                .tag("type", "all")
+                .register(meterRegistry)
+
+        finalVoteLatencyTimer =
+            Timer
+                .builder("election.vote.latency")
+                .tag("type", "final")
+                .register(meterRegistry)
     }
 
     @EventListener
@@ -32,10 +40,9 @@ class ElectionMetricProvider(
 
         val latency = Duration.between(event.transaction.receivedAt, event.timestamp)
 
+        voteLatencyTimer.record(latency)
         if (event.vote.isFinal()) {
-            lastFinalVoteLatency.set(latency)
-        } else {
-            lastVoteLatency.set(latency)
+            finalVoteLatencyTimer.record(latency)
         }
     }
 }
