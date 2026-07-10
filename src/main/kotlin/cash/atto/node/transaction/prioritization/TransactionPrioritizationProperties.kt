17fd3fe54e380cf9c056865841512000a7fc7957
rotilho <felipe@rotilho.com>
2026-07-10 12:28:08 +0200

    Improve transaction prioritization limits

--- src/main/kotlin/cash/atto/node/transaction/prioritization/TransactionPrioritizationProperties.kt
+++ src/main/kotlin/cash/atto/node/transaction/prioritization/TransactionPrioritizationProperties.kt
@@ -8,4 +8,15 @@ import org.springframework.context.annotation.Configuration
 class TransactionPrioritizationProperties {
     var groupMaxSize: Int? = null
     var maxActiveElections: Int? = null
+    var dependencyMaxSize: Int = 1_000
+        set(value) {
+            require(value > 0) { "dependencyMaxSize must be positive" }
+            field = value
+        }
+
+    var bufferMaxSize: Int = 10_000
+        set(value) {
+            require(value > 0) { "bufferMaxSize must be positive" }
+            field = value
+        }
 }
